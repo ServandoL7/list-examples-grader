8e43c2313f2507b2b313bc966e9ad90d554a6008
ServandoL7 <selara@ucsd.edu>
2023-05-25 09:14:08 -0700

    Updates

--- grade.sh
+++ grade.sh
@@ -8,26 +8,21 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
  
+if [[ ! $? =~ 0 ]]
+then
+    echo 'File Not Found. Check your file name.'
+    exit
+fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
 
-cd student-submission
-file = "ListExamples.java"
-    if [[ -f $file ]]
-    then
-        cp $file `grading-area`
-        cd ../grading-area
-        javac $file
-        java ${file%.java}
-        if [[ $? > 0 ]]
-        then
-            echo "Error within $file"
-        else
-            echo "pass"
-        fi
-    else
-        echo "None"
-    fi
+cp -r student-submission/ListExamples.java ListExamplesTests.java lib grading-area
+cd grading-area
+
+javac -cp $CPATH *.java ListExamplesTests > output.txt
+
+grade_line = "$(grep 'Tests run: ' output.txt | grep -o -E '[0-9]+')"
+echo $grade_line
